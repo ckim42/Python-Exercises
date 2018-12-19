d62690c7eb91cb1e63a3dc35b9ffc26bce0037c9
ilikecoffee18 <cherish.kim@students.makeschool.com>
2018-12-18 22:55:03 -0500

    first challenge complete

---
+++ helloworld.py
@@ -0,0 +1,27 @@
+# Using a function + return
+
+def sayHi():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    print(sayHi())
+
+
+# Using a function
+
+# def sayHi():
+#     print ("Hello, World!")
+#
+# if __name__ == '__main__':
+#     sayHi()
+
+
+# Using if name = main
+
+# if __name__ == '__main__':
+#     print("Hello, World!")
+
+
+# Simplest way
+
+# print ("Hello, World!")
