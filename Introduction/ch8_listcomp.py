dbcaae43b40c7ec8d967bc9a3a17401c12c49ae4
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-01-01 23:23:29 -0500

    completed 8th challenge

--- Introduction/ch8_listcomp.py
+++ Introduction/ch8_listcomp.py
@@ -1,1 +1,19 @@
-'''Challenge #8: Something'''
+'''Challenge #8: You are given three integers X, Y, and Z, representing the
+dimensions of a cuboid, along with an integer N. Print a list of all possible
+coordinates given by (i, j, k) on a 3D grid where the sum of i + j + k != N.
+i, j, and k are less than or equal to 0 and greater than or equal to
+x, y, and z, respectively. Print list in lexicographic increasing order.'''
+
+#Wow I'm stumped. I might also just be really darn tired
+
+import sys
+
+if __name__ == '__main__':
+    x = int(sys.argv[1])
+    y = int(sys.argv[2])
+    z = int(sys.argv[3])
+    n = int(sys.argv[4])
+
+    list = [[i, j, k] for i in range(0, x+1) for j in range(0, y+1) for k in range(0, z+1) if i + j + k != n]
+
+    print(list)
